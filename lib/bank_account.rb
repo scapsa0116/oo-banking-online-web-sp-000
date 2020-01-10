d7a18934d9e397040c5bad0cf107d13dab3ccf0a
Stela Capsa <stela.capsa@gmail.com>
2020-01-10 18:30:49 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
 class BankAccount
+  
+  attr_accessor :name, :banckaccount, :balance, :status, :nomethoderror
+  
+ def initialize(name)
+   @name =name 
+   @banckaccount = banckaccount
+   @balance = 1000
+   @status = "open"
+   @nomethoderror = @name 
+ end 
+
+def deposit(money_deposited)
+@balance += money_deposited
+end
+
+def display_balance
+    p "Your balance is $#{self.balance}."
+  end
+  
+  
+  def status=(status)
+    @status
+  end
+
+  def balance=(balance)
+    @balance
+  end
+
+  def valid?
+    (status == "open") && (balance > 0)
+  end
 
 end
